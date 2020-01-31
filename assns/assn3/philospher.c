d2aaa3032d787677b95052efe843adcff6c9a2d0
Victor Delaplaine <vdelaplaines@aol.com>
2020-01-31 01:01:20 -0800

    on a role need to debug and unit test* ;

--- assns/assn3/philospher.c
+++ assns/assn3/philospher.c
@@ -32,49 +32,211 @@
 
 
 #define NUM_PHILOSPHOPHERS 5
+#define LAST NUM_PHILOSPHOPHERS-1
 typedef enum STATES{EATING, THINKING, CHANGING} state_t;
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 
 int get_cycles(int argc, char *cycle);
 pthread_t *get_thread_by_label(char ascii, pthread_t *threads);
+void phil_life(void *id);
 
+/*========= Global var=============*/
+int cycles;/* Every thread has the same amount of cycles (so made global)*/
+sem_t forks[NUM_PHILOSPHOPHERS];/*num of forks = num of phils*/
+sem_t muxtexts[NUM_PHILOSPHOPHERS]; /* used to change the state*/
+sem_t print_lock; 
+#define UN_USED 1
+#define USED 0
+/*===============================*/
+
+
+/* WOULD NEED MUTEXT LOCKS IF KEEPING r_fork, and l_fork and state*/
+
+typedef struct philospher{
+    pthread_t thread; /* represents its session*/
+    sem_t *right, *left; /* For altering global fork before done*/
+    state_t state = CHANGING; /* state of the philospher*/
+}phils[NUM_PHILOSPHOPHERS];
+
+void build_header(int num_phils){
+    int i,j, per_col = 10;
+    const int size_sb = (NUM_PHILOSPHOPHERS*per_col)+(NUM_PHILOSPHOPHERS+1);
+    char sb_array[3][size_sb+1] = {'\0'}, *sb=sb_array[0];
+    /* ROW 1 or sb_array[0]*/
+    for(j=0; j < NUM_PHILOSPHOPHERS; j++){
+        *sb='|';
+        sb++;
+        for(i=0; i< per_col){
+            *sb='=';
+            sb++;
+        }
+    }
+    *sb='|';
+    /* ROW 2 or sb_array[1]*/
+    sb=sb_array[1];
+    for(j=0; j < NUM_PHILOSPHOPHERS; j++){
+        *sb='|';
+        sb++;
+        for(i=0; i< per_col){
+            if (i/2 == per_col){
+                *sb = j+'A';
+            }else{
+                *sb=' ';
+            }
+            sb++;
+        }
+    }
+    *sb='|';
+    /* ROW 3 or sb_array[2]*/
+    sb=sb_array[2];
+    for(j=0; j < NUM_PHILOSPHOPHERS; j++){
+        *sb='|';
+        sb++;
+        for(i=0; i< per_col){
+            *sb='=';
+            sb++;
+        }
+    }
+    *sb='|';
+
+}
 
 /*usage:  ./philospher [cycles]*/
 int main(int argc, char *argv[]){
-    int cycles, i;
+    int  i;
 
-    pthread_t *threads;
+
     if((cycles = get_cycles(argc, argv[1])) == -1){   
         fprintf(stderr, "%s", "Usage: ./philospher [Cycles]\n") /*flush stderr*/
         return;
     }
-    /*Cycles gonna be 0(no parm) or greater than zero(yes parm) */
+    /* Init the fork*/
+    for(i=0; i< NUM_PHILOSPHOPHERS; i++){
+        if(sem_init(&forks[i], 0, 1) <= -1){ /*each fork starting*/
+            perror('semaphore init problem');
+            exit(EXIT_FAILURE);
+        }
+        if(sem_init(&muxtexts[i], 0, 1) <= -1){ /*each fork starting*/
+            perror('semaphore init problem');
+            exit(EXIT_FAILURE);
+        }
 
-    if(!(threads = (pthread_t*)malloc(sizeof(pthread_t)* NUM_PHILOSPHOPHERS)){
-        exit(EXIT_FAILURE);
     }
-    for(i=0; i< NUM_PHILOSPHOPHERS; i++)
-        pthread_create(&threads[i], NULL, NULL, NULL);
+
+    /* Create new thread and init phils*/
+    for(i=0; i< NUM_PHILOSPHOPHERS; i++){
+        int right = (i+1)%NUM_PHILOSPHOPHERS;
+        int left = (i)%NUM_PHILOSPHOPHERS;
+        phils[i].state=CHANGING;
+        /* LAST SEMAPHORE IS DYSLEXIC*/
+        if(i == LAST){
+            /* TO AVOID DEAD LOCK(last philos takes right first) */
+            phils[i].right = &forks[l];
+            phils[i].left = &forks[r];
+        }else{
+            phils[i].right = &forks[r];
+            phils[i].left = &forks[l];
+        }
+        pthread_create(&phils[i], NULL, phil_life, i);
+    }
+
 
 
     // Initally they all start out starving (trying to go in eat state before)
 
     return 0;
 }
+/*==========SEMAPHORE HELPERS===================*/
+/* NOTE : THE LAST SEMAPHORE IS DYSLEXIC
+            (its right is the real left and vice versa)*/
+void down(sem_t *sema){
+    /*alter right to taken that is USED*/
+    sem_wait(sema);
+}
+void up(sem_t *sema){
+    /*alter right to taken that is UNUSED*/
+    sem_pos(sema);
+}
+
+void take_forks(int id){
+    /* Use fork */ 
+    down(&phils[id].left);
+    phils[id].state = CHANGING;
+    down(&phils[id].right);// inside down (changes right to used)
+    phils[id].state = EATING;
+}
+
+void give_forks(int id){
+    /* PUT BACK FORKS in the order we picked up*/
+    up(&phils[id].left);
+    phils[id].state = CHANGING;
+    up(&phils[id].right); /**(phils[id].left)=UN_USED;(inside up)*/
+    phils[id].state = THINKING;
+}
+/* Printing across*/
+void phil_print(int id){
+    ;
+}
+void phil_life(void *_id){
+    int id=*((char*)_id);
+    int i;
+
+    /* INITAL condition*/
+    for(i=0; i< cycles; i++){
+        /* Take forks(in function change philospher state)*/
+        take_forks(id)
+        eat(id);
+        /* Put down forks(change philosphers state*/
+        give_forks(id)
+        think(id);
+    }
+}
+void dawdle() {
+/*
+* sleep for a random amount of time between 0 and 999
+* milliseconds. This routine is somewhat unreliable, since it
+* doesn’t take into account the possiblity that the nanosleep
+* could be interrupted for some legitimate reason.
+*
+* nanosleep() is part of the realtime library and must be linked
+* with –lrt
+*/
+    struct timespec tv;
+    int msec = (int)(((double)random() / RAND MAX) * 1000);
+    tv.tv sec = 0;
+    tv.tv nsec = 1000000 * msec;
+    if ( −1 == nanosleep(&tv,NULL) ) {
+        perror("nanosleep");
+    }
+}
+void eat(int id){
+    /* Randomize*/
+    dawdle();
+}
+void think(int id){
+    dawdle();
+}
+
+
+//===========================================================//
 /* Description: helper to get optional cycles if it exist
         
-        Returns: {0 if no parm}
+        Returns: {1 if no parm}
                  {cycle if correct in put}
-                  { -1 cycles if invalid input}
+                 { -1 cycles if invalid input}
 */
 int get_cycles(int argc, char *cycle){
     int cycles;
     if (argc > 2){
         return -1;
     }else if (cycle == NULL){
-        return 0;
+        return 1;
     }else if(argc == 2 && !(cycles=atoi(cycle))){
         /*not a valid input*/
         return -1;
@@ -82,9 +244,9 @@ int get_cycles(int argc, char *cycle){
     return cycles;
 }
 /* Description: returns a thread given a label associated with it*/
-pthread_t *get_thread_by_label(char ascii, pthread_t *threads){
-    if(ascii < NUM_PHILOSPHOPHERS + 'a'){
-        return &threads[ascii-'a'];
+struct philoser *get_phil_by_label(char ascii, struct philospher *threads){
+    if(ascii < NUM_PHILOSPHOPHERS + 'A' && ascii >= A){
+        return &threads[ascii-'A'];
     }
     return NULL;
 }
