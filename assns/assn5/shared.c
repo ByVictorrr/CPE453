f906e978fcc2f3d295a10acffff399e1f59e5fad
Victor Delaplaine <vdelaplaines@aol.com>
2020-03-07 10:41:46 -0800

    read_inode - make more general

--- assns/assn5/shared.c
+++ assns/assn5/shared.c
@@ -10,22 +10,69 @@
 #include "path.h"
 #define MIN(a,b) ((a) < (b) ? (a):(b))
 
+#define MASK_DIR  0040000
+#define MASK_O_R  0000400
+#define MASK_O_W  0000200
+#define MASK_O_X  0000100
+#define MASK_G_R  0000040
+#define MASK_G_W  0000020
+#define MASK_G_X  0000010
+#define MASK_OT_R 0000004
+#define MASK_OT_W 0000002
+#define MASK_OT_X 0000001
+#define GET_PERM(mode, mask, c) ( (((mode)&(mask)) == mask) ? c : '-' )
+
+/************************PRINT FUNCTION ************************************************/
+char *get_mode(uint16_t mode) 
+{
+   char* permissions = (char *) malloc(sizeof(char) * 11);
+   permissions[0] = GET_PERM(mode, MASK_DIR, 'd');
+   permissions[1] = GET_PERM(mode, MASK_O_R, 'r');
+   permissions[2] = GET_PERM(mode, MASK_O_W, 'w');
+   permissions[3] = GET_PERM(mode, MASK_O_X, 'x');
+   permissions[4] = GET_PERM(mode, MASK_G_R, 'r');
+   permissions[5] = GET_PERM(mode, MASK_G_W, 'w');
+   permissions[6] = GET_PERM(mode, MASK_G_X, 'x');
+   permissions[7] = GET_PERM(mode, MASK_OT_R, 'r');
+   permissions[8] = GET_PERM(mode, MASK_OT_W, 'w');
+   permissions[9] = GET_PERM(mode, MASK_OT_X, 'x');
+
+   permissions[10] = '\0';
+   return permissions;
+}
+
+void print_dir(dirent_t *dir, minix_t minix, int size){
+    int i, inode_num;
+
+    for(i=0; i< size; i++){
+        minix.inodes[dir[i].inode];
+    }
+    return NULL;
+
+}
 
 
-/************************USING ALL ABOVE ************************************************/
+/************************PRINT FUNCTION ************************************************/
 
 
-// can pass a dir / char type 
-dirent_t * read_dir( const minix_t minix,
-                     const inode_t from,
-                     FILE * image
+typedef enum{REGULAR, DIRECTORY} file_t;
+// returns the data blocks associated with that inode (from)
+void * read_inode(const minix_t minix,
+                  const inode_t from,
+                  file_t type,
+                  FILE * image
                      ){
-    // how many bytes a zone takes up
     int ZONE_SIZE = minix.sb.blocksize << minix.sb.log_zone_size;     
     uint8_t i, data_index, b_left;
+    void *data = safe_calloc(from.size, sizeof(uint8_t));
     int type_size;
-    dirent_t *data = safe_calloc(from.size, sizeof(uint8_t));
-    // go through direct each zone 
+
+    // step 1 - determine if inode is dirent or reg file
+    if(type==DIRECTORY){
+       type_size = sizeof(dirent_t);
+    }else{
+        type_size = sizeof(uint8_t);
+    }
 
     b_left = from.size;
     for (i=0; i< DIRECT_ZONES && b_left; i++){
@@ -38,12 +85,14 @@ dirent_t * read_dir( const minix_t minix,
         }
         safe_fseek(image,
             minix.part.lFirst*SECTOR_SIZE + // start of part
-            from.zone[i]*ZONE_SIZE, // zone
+            from.zone[i]*ZONE_SIZE, // zone in bytes
             SEEK_SET
         );
-        safe_fread(data + data_index, sizeof(dirent_t), read_size, image);
+        safe_fread(data + data_index, type_size, read_size, image);
         b_left = b_left-read_size;
     }
+
+    // TODO : the other zones
     return data;
 }
 
@@ -76,7 +125,7 @@ inode_t Find_File( const minix_t minix,
         // step 1 - from inode
         curr = minix.inodes[inode_num];
         // step 2 - get the directory from that inode
-        entrys = read_dir(minix, curr, image);
+        entrys = read_inode(minix, curr, DIRECTORY, image);
         // step 3 - find that next dir in data
         char next_path[1000] = {0}, *next_folder_base;
         // step 4 - get the next directories base name
@@ -106,9 +155,13 @@ inode_t find_file(minix_t minix, FILE *image, char *file_path){
 }
 
 
+/**
+ * This function is used for ls minix 
+ */
 void read_file(FILE *image, char * src_path, int prim_part, int sub_part){
     uint32_t start_addr; // in bytes
     inode_t dest; // destination data files
+    uint8_t *data;
     minix_t minix;
     // step 1 - get minix part
     minix.part = find_minix_partion(image, prim_part, sub_part);
@@ -120,8 +173,8 @@ void read_file(FILE *image, char * src_path, int prim_part, int sub_part){
     minix.inodes = get_inodes(image, start_addr, minix.sb);
     // step 5 - read file of src_path
     dest = find_file(minix, image, src_path);
-    // step 6 - print out contents of that inode
-
+    // step 6 - print out contents of that inode (this is for reading for the get)
+    print_inode();
 }
 /**********************************************************************************/
 
