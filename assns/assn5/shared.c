c04a855a0552d4a48113ae58a111396b662c7efd
Victor Delaplaine <vdelaplaines@aol.com>
2020-03-06 17:40:29 -0800

    reformated

--- assns/assn5/shared.c
+++ assns/assn5/shared.c
@@ -6,185 +6,94 @@
 #include <string.h>
 
 
-#include "super.h"
-#include "partition.h"
-#include "dirent.h"
-#include "inode.h"
+#include "types.h"
 
-#define LENGTH 1000
-// In bytes
-#define SECTOR_SIZE 512
-// Location of partition table
-#define ADDR_PARTITION_TABLE  0x1BE
-// indicates a valid minix partition
-#define MINIX_PART 0x81
-// minix 3 magic number (little endian)
-#define MINIX_MAGIC 0x4D5A
-#define BOOT_SIZE 1024
-typedef enum BOOL{FALSE,TRUE}bool_t;
 
-// Wrapper function - so we dont have to worry about mess
-void safe_fseek(FILE *fp, long int offset, int pos){
-    if(fseek(fp, offset,  pos) != 0){
-        printf("seek error");
-        exit(EXIT_FAILURE);
+
+/************************USING ALL ABOVE ************************************************/
+
+void print_data(unsigned char * arr, int size){
+    int i;
+    for (i=0; i < size; i++){
+        //printf("%c", (char)arr[i]);
     }
 }
-// Wrapper function - so we dont have to worry about mess
-void safe_fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
-    if(fread(ptr, size,nmemb, stream) < 0){
-        printf("read error");
-        exit(EXIT_FAILURE);
+uint8_t * read_data( const minix_t minix,
+                     const inode_t from,
+                     FILE * image
+                     ){
+    // how many bytes a zone takes up
+    int ZONE_SIZE = minix.sb.blocksize << minix.sb.log_zone_size;     
+    uint8_t i, data_index, b_left;
+    uint8_t *data = safe_malloc(sizeof(uint8_t)*from.size);
+    // go through direct each zone 
+    b_left = from.size;
+    for (i=0; i< DIRECT_ZONES && b_left; i++){
+        // index where we are in reading data
+        data_index = from.size - b_left;
+        safe_fseek(image,
+            minix.part.lFirst*SECTOR_SIZE +
+            from.zone[i]*ZONE_SIZE,
+            SEEK_SET
+        );
+        safe_fread(data + data_index, sizeof(uint8_t), ZONE_SIZE, image);
+        b_left = b_left-ZONE_SIZE;
     }
+    print_data(data, data_index+1);
+
 }
-// Wrapper function - so we dont have to worry about mess
-void *safe_malloc(size_t size){
-    void *ptr;
-    if((ptr=malloc(size))==NULL){
-        printf("malloc error");
-        exit(EXIT_FAILURE);
-    }
-    return ptr;
-}
-
-/*******************************PARITION FUNCTIONS*******************************************/
-/* 
-* This function checks to see if the partion table is valid
-* @parm image - file pointer to image
-* @parm start_par - absolute address of start of current partition
-* @return - if the part table is valid true else false 
-*/
-bool_t is_part_table_valid(FILE *image, uint32_t start_addr){
-    const int VALID_SIG[2][2]={{510, 0x55},{511, 0xAA}};
-    uint8_t sig;
-    // step 1 - check at byte 510
-    safe_fseek(image, VALID_SIG[0][0]+start_addr, SEEK_SET);
-    safe_fread(&sig, sizeof(uint8_t), 1, image);
-    if(sig != VALID_SIG[0][1]){
-        printf("location %d doesnt have signature of %d", VALID_SIG[0][0], VALID_SIG[0][1]);
-        return FALSE;
-    }    
-    // step 2 - check at byte 511
-    safe_fseek(image, VALID_SIG[1][0]+start_addr, SEEK_SET);
-    safe_fread(&sig, sizeof(uint8_t), 1, image);
-    if(sig != VALID_SIG[1][1]){
-        printf("location %d doesnt have signature of %d", VALID_SIG[1][0], VALID_SIG[1][1]);
-        return FALSE;
-    } 
-    return TRUE;
-}
-
-/* 
-* Checks to see if the partion is bootable
-* @parm - addr absolute address to get parition
-*/
-partition_t get_partition(FILE *image, uint32_t addr){
-    partition_t part;
-    safe_fseek(image, addr, SEEK_SET);
-    safe_fread(&part, sizeof(part),1, image);
-    return part;
-}
-
-partition_t read_partition(FILE * image, uint32_t abs_addr, uint32_t abs_start_addr){
-    partition_t part;
-    // step 1 - make sure its valid
-    if(!is_part_table_valid(image, abs_start_addr)){
-        printf("Not a valid partition table");
-        exit(EXIT_FAILURE);
-    }
-    if((part = get_partition(image, abs_addr)).type != MINIX_PART){
-        printf("not a minix partition");
-        exit(EXIT_FAILURE);
-    }
-    return part;
-}
-
-/* 
-* This function return a valid minix partition object
-* @param prim_part - what primary partition to read
-* @param sub_part - what subpartion with a prim_part to read (-1 means no subpartion)
-* @return minix partition or null if table not valid
-
-*/
-partition_t find_minix_partion(FILE *image, int prim_part, int sub_part){
-    partition_t part;
-    const uint32_t prim_part_addr = ADDR_PARTITION_TABLE + 
-                            sizeof(partition_t) * prim_part;
-    uint32_t sub_part_addr;
-    // step 1 - get primary parition
-    part=read_partition(image, prim_part_addr, 0);
-    // step 2 - get sub partition is asked for
-    if(sub_part != -1){
-        sub_part_addr = (part.lFirst*SECTOR_SIZE) // this quanity is starting of partition table
-                + ADDR_PARTITION_TABLE + sizeof(partition_t)*sub_part;
-        part=read_partition(image, sub_part_addr, part.lFirst*SECTOR_SIZE);
-    }
-
-    return part;
-}
-
-/**********************************************************************************/
-/******************************SUPER BLOCK*******************************************/
-
-
-superblock_t get_SB(FILE *image, const uint32_t first_sector){
-    uint32_t LOC = first_sector+BOOT_SIZE;
-    superblock_t sb;
-    safe_fseek(image, LOC, SEEK_SET);
-    safe_fread(&sb, sizeof(superblock_t), 1, image);
-    return sb;
-}
-
-/******************************INODE BLOCK*******************************************/
-/**
- * @parm image file pointer object
- * @parma first_sector absolute value where first sector is
- * @parma sum_blk_maps - sum of sizes of zone and inode maps
- */
-inode_t *get_inodes(FILE *image,const uint32_t first_sector, superblock_t sb){
-    uint32_t LOC = first_sector+BOOT_SIZE+(sb.z_blocks+sb.i_blocks)*sb.blocksize;
-    inode_t *inodes = safe_malloc(sizeof(inode_t)*sb.ninodes);
-    safe_fseek(image, LOC, SEEK_SET);
-    safe_fread(inodes, sizeof(inode_t), sb.ninodes, image);
-    return inodes; 
-}
-
-
-/**********************************************************************************/
-
 
 
 /**
  * recursive function that goes through directies until it finds the src_path
  * returns the inode corresponding to the file_path
  */
-inode_t find_file(inode_t *inodes, int inode_num, const char *file_path, FILE * image){
+inode_t Find_File( const minix_t minix,
+                    int inode_num, 
+                    const char *file_path,
+                    FILE * image
+                    ){
     
-    dirent_t dir;
+    #define MAX 60
+    inode_t curr;
+    uint8_t *data;
     // base case stop when we find the base name 
     if(strcmp(file_path, basename(file_path)) == 0){
-        return inodes[inode_num];
+        return minix.inodes[inode_num];
+    }else{
+        // step 1 - from inode
+        curr = minix.inodes[inode_num];
+        // step 2 - get the directory from that inode
+        data = read_data(minix, curr, image);
+        // step 3 - memset it into the dir entry
+
+        
     }
-    // general case
     //safe_fseek(image, inodes[inode_num], )
 }
 
+
+// wrapper function of Find file_file
+inode_t find_file(minix_t minix, FILE *image, const char *file_path){
+    inode_t found = Find_File(minix, 0, file_path, image);
+    return found;
+}
+
+
 void read_file(FILE *image, const char * src_path, int prim_part, int sub_part){
-    partition_t minix_part;
-    superblock_t sb;
-    inode_t *inodes; // need to malloc doesnt know how many inodes
-    uint32_t addr_first_sector;
+    uint32_t start_addr; // in bytes
+    inode_t dest; // destination data files
+    minix_t minix;
     // step 1 - get minix part
-    minix_part = find_minix_partion(image, prim_part, sub_part);
+    minix.part = find_minix_partion(image, prim_part, sub_part);
     // absolute first sector of given partition in bytes
-    addr_first_sector=minix_part.lFirst*SECTOR_SIZE;
+    start_addr=minix.part.lFirst*SECTOR_SIZE;
     // step 2 - locate the relative address of superblock
-    sb=get_SB(image, addr_first_sector);
+    minix.sb=get_SB(image, start_addr);
     // step 3 - skip over inode bit map and zone bit map to get inodes 
-    inodes = get_inodes(image, addr_first_sector, sb);
-    // step 4 - determine if the src_path is a file or directory
-    // data_block_addr = find_file(inodes, 0, src_path, image);
-    
+    minix.inodes = get_inodes(image, start_addr, minix.sb);
+    // step 5 - read file of src_path
+    dest = find_file(minix, image, src_path);
 
 
 }
---
+++ assns/assn5/types.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include <string.h>
+
+#include "types.h"
+
+
+// Wrapper function - so we dont have to worry about mess
+void safe_fseek(FILE *fp, long int offset, int pos){
+    if(fseek(fp, offset,  pos) != 0){
+        printf("seek error");
+        exit(EXIT_FAILURE);
+    }
+}
+// Wrapper function - so we dont have to worry about mess
+void safe_fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
+    if(fread(ptr, size,nmemb, stream) < 0){
+        printf("read error");
+        exit(EXIT_FAILURE);
+    }
+}
+// Wrapper function - so we dont have to worry about mess
+void *safe_malloc(size_t size){
+    void *ptr;
+    if((ptr=malloc(size))==NULL){
+        printf("malloc error");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
+
+
+/*******************************PARITION FUNCTIONS*******************************************/
+/* 
+* This function checks to see if the partion table is valid
+*/
+bool_t is_part_table_valid(FILE *image, uint32_t table_addr){
+    const int VALID_SIG[2][2]={{510, 0x55},{511, 0xAA}};
+    uint8_t sig;
+    // step 1 - check at byte 510
+    safe_fseek(image, VALID_SIG[0][0]+table_addr, SEEK_SET);
+    safe_fread(&sig, sizeof(uint8_t), 1, image);
+    if(sig != VALID_SIG[0][1]){
+        printf("location %d doesnt have signature of %d", VALID_SIG[0][0], VALID_SIG[0][1]);
+        return FALSE;
+    }    
+    // step 2 - check at byte 511
+    safe_fseek(image, VALID_SIG[1][0]+table_addr, SEEK_SET);
+    safe_fread(&sig, sizeof(uint8_t), 1, image);
+    if(sig != VALID_SIG[1][1]){
+        printf("location %d doesnt have signature of %d", VALID_SIG[1][0], VALID_SIG[1][1]);
+        return FALSE;
+    } 
+    return TRUE;
+}
+
+/* 
+* Given and image turn the data at location addr in partition_t
+*/
+partition_t get_partition(FILE *image, uint32_t addr){
+    partition_t part;
+    safe_fseek(image, addr, SEEK_SET);
+    safe_fread(&part, sizeof(part),1, image);
+    return part;
+}
+/* Used to get if the table is */
+partition_t read_partition(FILE * image, uint32_t part_table, uint32_t im_addr){
+    partition_t part;
+    // step 1 - make sure its valid
+    if(!is_part_table_valid(image, part_table)){
+        printf("Not a valid partition table");
+        exit(EXIT_FAILURE);
+    }
+    if((part = get_partition(image, im_addr)).type != MINIX_PART){
+        printf("not a minix partition");
+        exit(EXIT_FAILURE);
+    }
+    return part;
+}
+
+/* 
+ Main function for this section, it returns a valid partition or exits (throws msg) 
+*/
+partition_t find_minix_partion(FILE *image, int prim_part, int sub_part){
+    partition_t part;
+    // step 1 : put @parm2 offset from @parm3 st start of prim part
+    part=read_partition(image, 
+        0,
+        ADDR_PARTITION_TABLE +  sizeof(partition_t) * prim_part
+    );
+    
+    // step 2 : do a similar thing for the sub_partition 
+    if(sub_part != -1){
+        // @parm2 is how far away if it away from its start addr(the partition)
+        part=read_partition(image
+            , part.lFirst*SECTOR_SIZE
+            , (part.lFirst*SECTOR_SIZE) + ADDR_PARTITION_TABLE
+                                    + sizeof(partition_t)*sub_part 
+          );
+    }
+
+    return part;
+}
+
+/*****************************EO PARTITION*************************************************/
+
+
+
+
+/******************************SUPER BLOCK*******************************************/
+
+/* Given an image at @parm2 start reading SB from there */
+superblock_t get_SB(FILE *image, const uint32_t first_sector){
+    uint32_t LOC = first_sector+BOOT_SIZE;
+    superblock_t sb;
+    safe_fseek(image, LOC, SEEK_SET);
+    safe_fread(&sb, sizeof(superblock_t), 1, image);
+    return sb;
+}
+
+/******************************INODE BLOCK*******************************************/
+/**
+ * @parm image file pointer object
+ * @parma first_sector absolute value where first sector is
+ * @parma sum_blk_maps - sum of sizes of zone and inode maps
+ */
+inode_t *get_inodes(FILE *image,const uint32_t first_sector, superblock_t sb){
+    uint64_t LOC = first_sector
+                    +(2+sb.z_blocks+sb.i_blocks)*sb.blocksize;
+    inode_t *inodes = safe_malloc(sizeof(inode_t)*sb.ninodes);
+    safe_fseek(image, LOC, SEEK_SET);
+    safe_fread(inodes, sizeof(inode_t), sb.ninodes, image);
+    return inodes; 
+}
